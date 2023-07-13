f7af96e4299fdb2af5fd0418eb7595fc4531e00d
Chris Pierson <cpierswim@gmail.com>
2023-07-13 14:53:31 -0400

    Added GroupAnanagram

--- LeetCode Tests/LeetCode Tests/TwoSumTests.cs
+++ LeetCode Tests/LeetCode Tests/TwoSumTests.cs
@@ -11,9 +11,9 @@ namespace LeetCode_Tests
     public class TwoSumTests
     {
         [Theory]
-        [InlineData(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1})]
-        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1,2 })]
-        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0,1 })]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
         [InlineData(new int[] { 2, 5, 5, 11 }, 10, new int[] { 1, 2 })]
         public void Tests(int[] nums, int target, int[] answer)
         {
---
+++ LeetCode Tests/LeetCodeSolutions/GroupAnagrams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCodeSolutions;
+
+
+namespace LeetCodeSolutions
+{
+    public class GroupAnagrams
+    {
+        public IList<IList<string>> Solution(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] arr = strs[i].ToCharArray();
+                Array.Sort(arr);
+                String sorted = new String(arr);
+
+                if (!dict.ContainsKey(sorted))
+                {
+                    dict.Add(sorted, new List<String>() { strs[i] });
+                }
+                else
+                {
+                    dict[sorted].Add(strs[i]);
+                }
+            }
+
+            return dict.Values.ToList();
+        }
+    }
+}
